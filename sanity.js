f64cd13a5aae12214a96de52e005b77e3e09193b
RKC3000 <ramakrishna.chhipa99@gmail.com>
2022-03-21 18:07:15 +0530

    Initital commit after header part is done

---
+++ sanity.js
@@ -0,0 +1,13 @@
+import {
+  createImageUrlBuilder,
+  createCurrentUseHook,
+  createClient,
+} from 'next-sanity'
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-03-25',
+
+  useCdn: process.env.NODE_ENV === 'production',
+}
